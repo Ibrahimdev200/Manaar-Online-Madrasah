8dbf0842f39bdde22815625343324f6a550bb7a6
Ibrahimdev200 <ibrahimdev200@gmail.com>
2024-08-25 00:16:32 -0700

    55% done

--- script.js
+++ script.js
@@ -157,3 +157,30 @@ function updatemenu() {
         document.getElementById('thank-you').classList.remove('hidden');
     }, 2000);
 }
+
+
+
+const scrollElements = document.querySelectorAll('.scroll-reveal');
+
+const elementInView = (el, dividend = 1) => {
+    const elementTop = el.getBoundingClientRect().top;
+    return (
+        elementTop <= (window.innerHeight || document.documentElement.clientHeight) / dividend
+    );
+};
+
+const displayScrollElement = (element) => {
+    element.classList.add('revealed');
+};
+
+const handleScrollAnimation = () => {
+    scrollElements.forEach((el) => {
+        if (elementInView(el, 1.25)) {
+            displayScrollElement(el);
+        }
+    });
+};
+
+window.addEventListener('scroll', () => {
+    handleScrollAnimation();
+});
